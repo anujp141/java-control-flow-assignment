b067d15c8c81efb5500b837b34b5e156ecc1ee3e
anujp141 <134326334+anujp141@users.noreply.github.com>
2023-08-26 17:01:32 -0400

    Update GradeCalculator.java

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -2,40 +2,44 @@ import java.util.Scanner;
 
 public class GradeCalculator {
 	public static void main(String[] args) {
-		// 1. Initialize any variables and objects you need (i.e. a scanner, some
-		// counters for a total running sum, and a number tracking the # of scores inputted
+		 Scanner scanner = new Scanner(System.in);
+        int totalSum = 0;
+        int numOfScores = 0;
 
-		
-		
-		
-		
-		// 2. Make a loop that repeatedly asks the user for a number and won't stop
-		// until the user answers using a negative number
-		// 3. Add some error checking for when a user inputs a number over 100 - we
-		// should skip that input and start the loop
-		// over to ask again
+        System.out.println("Enter scores (enter a negative number to stop):");
 
-		
-		
-		
-		
-		// 4. Calculate the average of all scores collected during the loop
+        while (true) {
+            System.out.print("Enter a score: ");
+            int score = scanner.nextInt();
 
-		
-		
-		
-		
-		
-		
-		// 5. Display the average grade and corresponding message
-		// - If over 90: "Excellent Work!"
-		// - If over 80: "Good Job!"
-		// - If over 70: "Keep it up!"
-		// - anything else: "Let's work hard to get those grades up!"
+            if (score < 0) {
+                break;
+            } else if (score > 100) {
+                System.out.println("Invalid score! Please enter a score between 0 and 100.");
+                continue;
+            }
 
-		
-		
-		
-		
-	}
+            totalSum += score;
+            numOfScores++;
+        }
+
+        scanner.close();
+
+        if (numOfScores > 0) {
+            double average = (double) totalSum / numOfScores;
+            System.out.println("Average score: " + average);
+
+            if (average > 90) {
+                System.out.println("Excellent Work!");
+            } else if (average > 80) {
+                System.out.println("Good Job!");
+            } else if (average > 70) {
+                System.out.println("Keep it up!");
+            } else {
+                System.out.println("Let's work hard to get those grades up!");
+            }
+        } else {
+            System.out.println("No scores entered.");
+        }
+    }
 }
